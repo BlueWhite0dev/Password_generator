e03872665b1333ca32159428b40cf745bd76f490
BlueWhite0dev <BlueWhite.dev@gmail.com>
2024-05-29 21:08:50 +0200

    Added password strength page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const inputs = document.querySelectorAll("input");
-let labels = document.querySelectorAll("label");
+const inputs = document.querySelectorAll("input[type='checkbox']");
+let labels = document.querySelectorAll(".options_container label");
 inputs.forEach((input, index) =>
   input.addEventListener("click", () => {
     labels[index].classList.toggle("disabled");
